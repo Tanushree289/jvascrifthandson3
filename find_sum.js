de1399cb54c93a028822777ede8c58b8fbd38ac1
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-10 16:56:21 +0530

    Add files via upload

---
+++ find_sum.js
@@ -0,0 +1,9 @@
+const findSum = (n) => {
+    var i,sum=0
+    for(i=2;i<=n;i=i+2)
+    {
+      sum=sum+i;
+    }
+      return sum;
+  };
+  
---
+++ printnumber_func.js
@@ -0,0 +1,9 @@
+const printNumbers = (n) => {
+    s=""
+    for(i=1;i<=n;i++)
+    {
+    s+=i;
+    s+=" ";
+    }
+    console.log(s)
+};
